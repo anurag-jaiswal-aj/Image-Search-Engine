ef35d5827dcb30060b33fb37418e415a73d17900
Anurag Jaiswal <janurag582004@gmail.com>
2024-01-30 09:47:28 +0530

    .

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let page = 1;
 async function searchImages() {
     keyword = searchBox.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
-
     const response = await fetch(url);
     const data = await response.json();
 
